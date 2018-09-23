00b144fb17bca9e5cdef1f7b4a2600a65ddaabce
OmerBlechman <37245909+OmerBlechman@users.noreply.github.com>
2018-09-23 19:23:30 +0300

    draw mode bug fixed

--- src/NinARowWeb/src/Game/Servlet/GameRoom/PlayerMoveServlet.java
+++ src/NinARowWeb/src/Game/Servlet/GameRoom/PlayerMoveServlet.java
@@ -24,20 +24,21 @@ public class PlayerMoveServlet extends HttpServlet {
         Board game = manager.getGameBoard(boardName);
         PrintWriter out = response.getWriter();
         if(SessionUtils.getAttribute(request,Constants.UNIQUE_ID).equals(Integer.toString(game.getCurrentPlayerUniqueID()))) {
-            boolean popout = request.getParameter("Popout").equals("true") ? true : false;
+            boolean popout = request.getParameter("Popout") == "True" ? true : false;
             int col = Integer.parseInt(request.getParameter("Col"));
             Point move = game.playerMove(col, popout);
+            out.print(SessionUtils.getAttribute(request, Constants.ERROR_LOGIN_MESSAGE));
             if (move == null) {
                 if (popout) {
                     out.print(String.format("Your popout move is not legal"));
                 } else {
-                    out.print(String.format("col: %d is fulled. please enter another col in range", (col + 1)));
+                    out.print(String.format("col: {0} is fulled. please enter another col in range", (col + 1)));
+
                 }
             }
         }
         else{
-            SessionUtils.setAttribute(request,Constants.PLAYER_TURN_ERROR,String.format("This is not your turn"));
-            out.print("This is not your turn");
+            out.print(String.format("This is not your turn"));
         }
     }
 }
